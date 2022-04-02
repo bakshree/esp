c0dce6a734dedb15baf72302b480623200413edb
Bakshree Mishra <bmishra3@illinois.edu>
2022-04-02 14:54:08 -0500

    Added tiled_acc_stratus init files

---
+++ accelerators/stratus_hls/tiled_acc_stratus/sw/linux/include/tiled_acc_stratus.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+#ifndef _TILED_ACC_STRATUS_H_
+#define _TILED_ACC_STRATUS_H_
+
+#ifdef __KERNEL__
+#include <linux/ioctl.h>
+#include <linux/types.h>
+#else
+#include <sys/ioctl.h>
+#include <stdint.h>
+#ifndef __user
+#define __user
+#endif
+#endif /* __KERNEL__ */
+
+#include <esp.h>
+#include <esp_accelerator.h>
+
+struct tiled_acc_stratus_access {
+	struct esp_access esp;
+	/* <<--regs-->> */
+	unsigned num_tiles;
+	unsigned tile_size;
+	unsigned src_offset;
+	unsigned dst_offset;
+};
+
+#define TILED_ACC_STRATUS_IOC_ACCESS	_IOW ('S', 0, struct tiled_acc_stratus_access)
+
+#endif /* _TILED_ACC_STRATUS_H_ */
