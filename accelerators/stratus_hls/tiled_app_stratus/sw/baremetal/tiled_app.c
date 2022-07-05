a6b736ee07760993803a76cf7ca8aa99b1fda6c8
Bakshree Mishra <bmishra3@illinois.edu>
2022-07-05 00:19:57 -0500

    Added asm blocks for coherence

--- accelerators/stratus_hls/tiled_app_stratus/sw/baremetal/tiled_app.c
+++ accelerators/stratus_hls/tiled_app_stratus/sw/baremetal/tiled_app.c
@@ -14,12 +14,93 @@
 
 typedef int64_t token_t;
 
+typedef union
+{
+  struct
+  {
+    unsigned char r_en   : 1;
+    unsigned char r_op   : 1;
+    unsigned char r_type : 2;
+    unsigned char r_cid  : 4;
+    unsigned char w_en   : 1;
+    unsigned char w_op   : 1;
+    unsigned char w_type : 2;
+    unsigned char w_cid  : 4;
+	uint16_t reserved: 16;
+  };
+  uint32_t spandex_reg;
+} spandex_config_t;
+
 static unsigned DMA_WORD_PER_BEAT(unsigned _st)
 {
         return (sizeof(void *) / _st);
 }
 
+
+// static void start_network_counter()
+// {
+//     uint64_t tile_id;
+//     for (tile_id = 0; tile_id < SOC_COLS * SOC_ROWS; tile_id++)
+//     {
+//         volatile void* monitor_base = (volatile void*)(0x60090000 + 0x200 * tile_id);
+//         volatile uint32_t* control = ((volatile uint32_t*)monitor_base) + 1; // ctrl_window_size_offset
+//         *control = 0xffffffff;
+//     }
+// }
+
+// static uint32_t get_network_counter(int x, int y, int plane)
+// {
+//     uint64_t tile_id = x + y * SOC_COLS;
+//     volatile void* monitor_base = (volatile void*)(0x60090000 + 0x200 * tile_id);
+//     volatile uint32_t* mon_register = ((volatile uint32_t*)monitor_base) + 4 + 22 + plane;
+//     return *mon_register;
+// }
+
+static uint64_t get_counter() {
+  uint64_t counter;
+  asm volatile (
+    "li t0, 0;"
+    "csrr t0, mcycle;"
+    "mv %0, t0"
+    : "=r" ( counter )
+    :
+    : "t0"
+  );
+
+  return counter;
+}
+
+uint64_t start_write;
+uint64_t stop_write;
+uint64_t intvl_write;
+uint64_t start_read;
+uint64_t stop_read;
+uint64_t intvl_read;
+
+uint64_t start_acc_write;
+uint64_t stop_acc_write;
+uint64_t intvl_acc_write;
+uint64_t start_acc_read;
+uint64_t stop_acc_read;
+uint64_t intvl_acc_read;
+
+uint64_t start_network_cpu[3];
+uint64_t stop_network_cpu[3];
+uint64_t intvl_network_cpu[3];
+uint64_t start_network_acc[3];
+uint64_t stop_network_acc[3];
+uint64_t intvl_network_acc[3];
+uint64_t start_network_llc[3];
+uint64_t stop_network_llc[3];
+uint64_t intvl_network_llc[3];
+
+
+token_t *mem;
+token_t *gold;
+token_t *out;
+
 // #define PRINT_DEBUG
+// #define VALIDATE
 #define SLD_TILED_APP 0x033
 #define DEV_NAME "sld,tiled_app_stratus"
 
@@ -37,6 +118,9 @@ static unsigned out_size;
 static unsigned out_offset;
 static unsigned mem_size;
 
+static unsigned	int read_tile ;
+static unsigned	int write_tile;
+
 static unsigned coherence;
 
 /* Size of the contiguous chunks for scatter/gather */
@@ -46,6 +130,51 @@ static unsigned coherence;
 			(_sz / CHUNK_SIZE) :		\
 			(_sz / CHUNK_SIZE) + 1)
 
+/* Coherence Modes */
+#define COH_MODE 2
+#define ESP
+
+#ifdef ESP
+// ESP COHERENCE PROTOCOLS
+#define READ_CODE 0x0002B30B
+#define WRITE_CODE 0x0062B02B
+#if(COH_MODE == 3)
+#define COHERENCE_MODE ACC_COH_NONE
+#elif (COH_MODE == 2)
+#define COHERENCE_MODE ACC_COH_LLC
+#elif (COH_MODE == 1)
+#define COHERENCE_MODE ACC_COH_RECALL
+#else
+#define COHERENCE_MODE ACC_COH_FULL
+#endif
+#else
+//SPANDEX COHERENCE PROTOCOLS
+#define COHERENCE_MODE ACC_COH_FULL
+#if (COH_MODE == 3)
+// Owner Prediction
+#define READ_CODE 0x4002B30B
+#define WRITE_CODE 0x2262B82B
+spandex_config_t spandex_config = {.spandex_reg = 0, .r_en = 1, .r_type = 2, .w_en = 1, .w_op = 1, .w_type = 1};
+#elif (COH_MODE == 2)
+// Write-through forwarding
+#define READ_CODE 0x4002B30B
+#define WRITE_CODE 0x2062B02B
+spandex_config_t spandex_config = {.spandex_reg = 0, .r_en = 1, .r_type = 2, .w_en = 1, .w_type = 1};
+#elif (COH_MODE == 1)
+// Baseline Spandex
+#define READ_CODE 0x2002B30B
+#define WRITE_CODE 0x0062B02B
+spandex_config_t spandex_config = {.spandex_reg = 0, .r_en = 1, .r_type = 1};
+#else
+// Fully Coherent MESI
+#define READ_CODE 0x0002B30B
+#define WRITE_CODE 0x0062B02B
+spandex_config_t spandex_config;
+#endif
+#endif
+
+
+
 /* User defined registers */
 /* <<--regs-->> */
 #define TILED_APP_NUM_TILES_REG 0x48
@@ -53,123 +182,70 @@ static unsigned coherence;
 #define TILED_APP_RD_WR_ENABLE_REG 0x40
 #define SYNC_BITS 1
 
-int load_mem(token_t *in, token_t *gold, int* tile){
-	// int offset = 2;
-#if 0
-	int offset = SYNC_BITS;
-	if(in[0]!=1 && *tile < num_tiles){
-		// printf("In  sync: %d Loading tile %d: \n",in[0], *tile);
-		for (int j = 0; j < tile_size; j++)
-			in[offset + j] = 2*((*tile) * in_words_adj + j);
-
-		// in[0] = 0;
-		// printf("Next data: ");
-		// for(int __in = 0; __in< 2+2*(tile_size); __in++)
-		// 	printf("%d ", in[__in]);
-		// printf("\n");
-        asm volatile ("fence rw, rw");
-
-		// printf("Then data: ");
-		// for(int __in = 0; __in< 2+2*(tile_size); __in++)
-		// 	printf("%d ", in[__in]);
-		// printf("\n");
-		// for(int i = 0; i< tile_size; i++)
-		// 	printf("%d, ", 2*((*tile) * in_words_adj + i));
-
-		// printf("\n");
-
-		in[0] = 1;
-
-		// asm volatile ("fence rw, rw");
-		*tile = *tile + 1;
-		return 1;
-	}
-#else
-	int offset = 0;// SYNC_BITS;
-
-	if(in[2*tile_size]!=1 && *tile < num_tiles){
-		for (int j = 0; j < tile_size; j++)
-			in[offset + j] = gold[(*tile)*out_words_adj + j];
-
-#ifdef PRINT_DEBUG
-		in[2*tile_size] = 0;
-		printf("Read  Tile %d:\t", *tile);
-		for(int __in = 0; __in< 2*(tile_size)+SYNC_BITS; __in++){
-			if(__in % tile_size == 0)printf("||\t");
-			printf("%d\t", in[__in]);
-		}
-		printf("\n");
-#endif
-		// Mem fence before updating Sync bit
-        asm volatile ("fence rw, rw");
-		// Update Sync bit
-		in[2*tile_size] = 1;
-		*tile = *tile + 1;
-		return 1;
-	}
-
-#endif
-	return 0;
+inline uint32_t read_sync(){
+	void* dst = (void*)(mem+2*tile_size);
+	int64_t value_64 = 0;
+	asm volatile (
+		"mv t0, %1;"
+		".word 0x0002B30B;"
+		"mv %0, t1"
+		: "=r" (value_64)
+		: "r" (dst)
+		: "t0", "t1", "memory"
+	);
+	return (value_64 == 2);
 }
 
+inline void update_load_sync(){
+	void* dst = (void*)(mem+2*tile_size);
+	int64_t value_64 = 1;
+	asm volatile (
+		"mv t0, %0;"
+		"mv t1, %1;"
+		".word 0x0062B02B"
+		: 
+		: "r" (dst), "r" (value_64)
+		: "t0", "t1", "memory"
+	);
+}
 
-#if 0
-int store_mem(token_t *out, token_t* mem, int offset, int* tile){
-	if(mem[1]!=0 && *tile < num_tiles){
-		printf("Out sync: %d Storing tile %d at offset %d\n", mem[1], *tile, (*tile) * out_words_adj);
-		for (int j = 0; j < tile_size; j++)
-			out[(*tile) * out_words_adj + j] = mem[offset + j];
+static inline void load_mem(){
+	void *dst = (void*)(mem);
+	for (int j = 0; j < tile_size; j++){
+		//int64_t value_64 = gold[(read_tile)*out_words_adj + j];
+		asm volatile (
+			"mv t0, %0;"
+			"mv t1, %1;"
+			".word 0x0062B02B"
+			: 
+			: "r" (dst), "r" (gold[(read_tile)*out_words_adj + j])
+			: "t0", "t1", "memory"
+		);
 
-        asm volatile ("fence rw, rw");
-		// for(int i = 0; i< tile_size; i++)
-		// 	printf("%d, ", mem[offset + i]);
-
-		// printf("\n");
-
-		// mem[0] = 0; //TODO: delete
-		mem[1] = 0;
-
-        // asm volatile ("fence rw, rw");
-		*tile = (*tile) + 1;
-		return 1;
+		dst += 8;
 	}
-#else
-int store_mem(token_t *out, token_t* mem, int offset, int* tile, int* read_tile, token_t *gold){
-	if(mem[2*tile_size]!=0 && mem[2*tile_size]!=1 && *tile < num_tiles){
-// #ifdef PRINT_DEBUG
-// 		printf("Out sync: %d Storing tile %d at offset %d\n", mem[1], *tile, (*tile) * out_words_adj);
-// #endif
+	asm volatile ("fence rw, rw");
+	read_tile += 1;
+}
 
-		// Copy Store tile from Shared Memory
-		for (int j = 0; j < tile_size; j++)
-			out[(*tile) * out_words_adj + j] = mem[offset + j];
-
-		// Mem fence before updating Store Sync Bit
-        asm volatile ("fence rw, rw");
-		// Temporarily removed Store Sync for Single Sync Bit
-
-// #ifdef PRINT_DEBUG
-// 		for(int i = 0; i< tile_size; i++)
-// 			printf("%d, ", mem[offset + i]);
-// 		printf("\n");
-// #endif
-
-#ifdef PRINT_DEBUG
-		printf("Store Tile %d:\t", *tile);
-		for(int __in = 0; __in< 2*(tile_size)+SYNC_BITS; __in++){
-			if(__in % tile_size == 0)printf("||\t");
-			printf("%d\t", mem[__in]);
-		}
-		printf("\n");
-#endif
-
-		*tile = (*tile) + 1;
-		if(*read_tile < num_tiles) load_mem(mem, gold, read_tile);
-
-		return 1;
-	}	
-#endif
-	return 0;
+static inline void store_mem(){
+	void *src = (void*)(mem+tile_size);
+	// out [i] = mem[j];
+	int64_t out_val;
+	for (int j = 0; j < tile_size; j++){
+		//int64_t value_64 = gold[(read_tile)*out_words_adj + j];
+		asm volatile (
+			"mv t0, %1;"
+			".word 0x0002B30B;"
+			"mv %0, t1"
+			: "=r" (out[(write_tile) * out_words_adj + j])
+			: "r" (src)
+			: "t0", "t1", "memory"
+		);
+		src += 8;
+	}
+	asm volatile ("fence rw, rw");
+	write_tile++;
 }
 
 static int validate_buf(token_t *out, token_t *gold)
@@ -178,7 +254,6 @@ static int validate_buf(token_t *out, token_t *gold)
 	int j;
 	unsigned errors = 0;
 	int loc = 0;
-	printf("num tiles %d -- tile size %d \n", num_tiles, tile_size);
 	for (i = 0; i < num_tiles; i++)
 		for (j = 0; j < tile_size; j++){
 			if (gold[i * out_words_adj + j] != out[i * out_words_adj + j]){
@@ -211,15 +286,44 @@ static void init_buf (token_t *in, token_t * gold, token_t* out)
 		}
 		
 	}
-	
-    // asm volatile ("fence rw, rw");
-	printf("Init buf complete\n");
+}
+
+
+/* Print utilities
+*/
+inline void console_log_header(){
+	int left = (tile_size%2)?tile_size/2+1 : tile_size/2;
+	int right = tile_size/2;
+
+	printf("Tile Size : %d\t||", tile_size);
+	for (int __i = 0; __i<left; __i++)printf("\t");
+	printf("READ TILE");
+	for (int __i = 0; __i<right; __i++)printf("\t");
+	printf("||");
+	for (int __i = 0; __i<left; __i++)printf("\t");
+	printf("STORE TILE");
+	for (int __i = 0; __i<right; __i++)printf("\t");
+	printf("||SYNC VAR\n");
+}
+inline static void print_mem(int read ){
+	if(read == 1)
+	printf("Read  Tile: %d\t||", read_tile);
+	else
+	printf("Store Tile: %d\t||", write_tile);
+	for(int __i = 0; __i < tile_size; __i++)
+		printf("\t%d", mem[__i]);
+	printf("\t||");
+	for(int __i = 0; __i < tile_size; __i++)
+		printf("\t%d", mem[tile_size+__i]);
+	printf("\t||\t%d\n", mem[2*tile_size]);
 }
 
 
 int main(int argc, char * argv[])
 {
 	// printf("Hello World 123\n");
+	int32_t num_tiles = 10;//12;
+	int32_t tile_size = 7;
 
 	#if 1
 	int i;
@@ -229,9 +333,6 @@ int main(int argc, char * argv[])
 	struct esp_device *dev;
 	unsigned done;
 	unsigned **ptable;
-	token_t *mem;
-	token_t *gold;
-	token_t *out;
 	unsigned errors = 0;
 	// printf("Checkpoint 1\n");
 
@@ -254,15 +355,8 @@ int main(int argc, char * argv[])
 	// printf("Checkpoint 3\n");
 
 
-	printf("num_tiles      =  %u\n ",num_tiles     	); //num_tiles);
-	printf("in_words_adj   =  %u\n ",in_words_adj   ); //num_tiles);
-	printf("out_words_adj  =  %u\n ",out_words_adj  ); //num_tiles);
-	printf("in_len         =  %u\n ",in_len     	); //num_tiles);
-	printf("out_len        =  %u\n ",out_len   		); // (num_tiles);
-	printf("in_size        =  %u\n ",in_size    	); //(token_t);
-	printf("out_size       =  %u\n ",out_size   	); //of(token_t);
-	printf("out_offset     =  %u\n ",out_offset 	); //
-	printf("mem_size       =  %u\n ",mem_size   	); //sizeof(token_t)) + out_size;
+	printf("num_tiles      =  %u\n",num_tiles     	); //num_tiles);
+	printf("tile_size      =  %u\n",tile_size	    ); //num_tiles);
 
 	// Search for the device
 	printf("Scanning device tree... \n");
@@ -309,12 +403,17 @@ int main(int argc, char * argv[])
 		printf("  nchunk = %lu\n", NCHUNK(mem_size));
 
         asm volatile ("fence rw, rw");
+
+		// If Spandex Caches
+#ifndef ESP
+			iowrite32(dev, SPANDEX_REG, spandex_config.spandex_reg);
+#endif
 		//#ifndef __riscv
 		// 		for (coherence = ACC_COH_NONE; coherence <= ACC_COH_FULL; coherence++) {
 		// #else
 		{
 			/* TODO: Restore full test once ESP caches are integrated */
-			coherence = ACC_COH_RECALL; //ACC_COH_FULL
+			coherence = COHERENCE_MODE ; //ACC_COH_RECALL; //ACC_COH_FULL
 		// #endif
 			printf("  --------------------\n");
 			printf("  Generate input...\n");
@@ -350,72 +449,83 @@ int main(int argc, char * argv[])
 			// Start accelerators
 			printf("  Start...\n");
 			iowrite32(dev, CMD_REG, CMD_MASK_START);
-			int read_tile = 0;
-			int write_tile = 0;
+			read_tile = 0;
+			write_tile = 0;
 			// Wait for completion
 			done = 0;
 			int done_prev = 0;
 			int temp_rd_wr_enable = 0;
 			int store_done_prev = 0;
         	int load_turn = 1;
-			// int tiles_read = 0;
-			// while ( write_tile < 7) {
-			// printf("tiles: %d load_turn: %d in_sync:%d out_sync:%d read_tile:%d write_tile: %d done: %d\n",num_tiles, load_turn, mem[0], mem[1], read_tile, write_tile, done);
-	#ifdef PRINT_DEBUG
-			// printf("Tile Size : %d\t||\t\t\t\tREAD TILE\t\t\t\t||\t\t\t\tSTORE TILE\t\t\t\t||SYNC BIT\n", tile_size);
-			int left = (tile_size%2)?tile_size/2+1 : tile_size/2;
-			int right = tile_size/2;
-
-			printf("Tile Size : %d\t||", tile_size);
-			for (int __i = 0; __i<left; __i++)printf("\t");
-			printf("READ TILE");
-			for (int __i = 0; __i<right; __i++)printf("\t");
-			printf("||");
-			for (int __i = 0; __i<left; __i++)printf("\t");
-			printf("STORE TILE");
-			for (int __i = 0; __i<right; __i++)printf("\t");
-			printf("||");
-			printf("SYNC VAR\n");
-	#endif
-			load_mem(mem, gold, &read_tile); 
-			// while ( write_tile < num_tiles) {
+#ifdef PRINT_DEBUG
+			console_log_header();
+#endif
+			//load_mem(mem, gold, &read_tile); 
+			void* dst = (void*)(mem);
+			start_write = get_counter();
+			start_acc_write = start_write;
+			// Load 1st Tile
+			load_mem();
+#ifdef PRINT_DEBUG
+			print_mem(1);
+#endif
+			update_load_sync();
+			stop_write = get_counter();
+			intvl_write = stop_write - start_write;
+			intvl_read = 0;
 			while ( !done ) {
 				done = ioread32(dev, STATUS_REG);
-
+				int32_t read_done = read_sync();
+				if(read_done==1){
+					start_read = get_counter();
+					store_mem();
+					stop_read = get_counter();
+					intvl_read += stop_read - start_read;
 #ifdef PRINT_DEBUG
-				printf("tiles: %d load_turn: %d in_sync:%d out_sync:%d read_tile:%d write_tile: %d done: %d\n",num_tiles, load_turn, mem[0], mem[1], read_tile, write_tile, done);
-				for(int in = 0; in< (in_len+out_len); in++)
-					printf("%d ", mem[in]);
-				printf("\n");
+					print_mem(0);
 #endif
-
-				store_mem(out, mem, tile_size, &write_tile, &read_tile, gold);
+					if(read_tile<num_tiles){
+						start_write = get_counter();
+						load_mem();
+#ifdef PRINT_DEBUG
+						print_mem(1);
+#endif
+						update_load_sync();
+						stop_write = get_counter();
+						intvl_write += stop_write - start_write;
+					}
+				}
 				done &= STATUS_MASK_DONE;
-				// asm volatile ("fence rw, rw");
-
 			}
-			if(write_tile < num_tiles) store_mem(out, mem, tile_size, &write_tile, &read_tile, gold);
-			//What is this doing?
-			// iowrite32(dev, CMD_REG, 0x0);
+			//Fetch last tile
+			if(write_tile < num_tiles) {
+					start_read = get_counter();
+					store_mem();
+					stop_read = get_counter();
+					intvl_read += stop_read - start_read;
 #ifdef PRINT_DEBUG
-			// printf("Load Done bit: %d\n", mem[0]);
-			// printf("Store Done bit: %d\n", mem[1]);
-			// for(int i = 0; i< 2*tile_size + SYNC_BITS; i++)
-			// 	printf("mem bit %d: %d\n", i, mem[i]);
-			// for(int i = 0; i< num_tiles*tile_size; i++)
-			// 	printf("out bit %d: %d\n", i, out[i]);
+					print_mem(0);
 #endif
-			
+			}
+			stop_acc_write = stop_read;
+			intvl_acc_write = stop_acc_write - start_acc_write;
+			//What is this doing?
+			iowrite32(dev, CMD_REG, 0x0);
+
 			printf("  Done\n");
+#ifdef VALIDATE
 			printf("  validating...\n");
 
 			/* Validation */
-			//errors = validate_buf(&mem[out_offset], gold);
 			errors = validate_buf(out, gold);
 			if (errors)
 				printf("  ... FAIL\n");
 			else
 				printf("  ... PASS\n");
+#endif
+			printf("  Tile 	Read Time: %lu\n", intvl_read		);
+			printf("  Tile Write Time: %lu\n", intvl_write		);
+			printf("  Total Tile Time: %lu\n", intvl_acc_write	);
 		}
 		aligned_free(out);
 		aligned_free(ptable);
